0ee6dfdba028b34351d7570fc64f3fa840bfc3d1
misterkon <haakonhh7@gmail.com>
2023-08-08 19:54:14 +0200

    oops vercel

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/entries/pages/_page.svelte.js
+++
@@ -1,7 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/ssr.js";
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<div class="card card-hover items-center justify-center m-auto mt-20 w-2/3 border-4 rounded border-slate-600 h-2/3 variant-glass " data-svelte-h="svelte-1wudnx9"><header class="card-header mb-10 mt-5 text-center lg:text-3xl md:text-lg text-white font-bold">Welcome to Homegame Tracker!</header> <div class="container w-full space-y-5"><div class="w-2/3 card flex justify-center m-auto border-2 rounded border-secondary-500 p-10"><p class="font-bold text-3xl italic">Poker pros only.</p></div> <div class="w-2/3 card flex justify-center m-auto border-2 rounded border-secondary-500 p-10"><p class="font-bold text-3xl italic text-primary-500">Nima suger</p></div></div></div>`;
-});
-export {
-  Page as default
-};
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.90cb9643.js","_app/immutable/chunks/scheduler.029ef716.js","_app/immutable/chunks/index.2a4c170a.js","_app/immutable/chunks/singletons.0a1b5808.js","_app/immutable/chunks/index.c24a7714.js"];
-export const stylesheets = [];
-export const fonts = [];
